2971c95b6558e48bd95930cceac0a489aecf831b
Jonas Elvedal Hole <jolejonaseh@gmail.com>
2020-09-30 12:24:03 +0200

    abstraction and cleanup

--- main.cpp
+++ main.cpp
@@ -82,6 +82,50 @@ void react(std::shared_ptr<DppBot> &bot, string channel_id, string message_id, s
 		nullptr, nullptr, nullptr);
 }
 
+struct Attachment
+{
+	string url, filename;
+	long size, width, height;
+	Attachment(json attach)
+	{
+		url	= attach["url"].get<string>();
+		filename= attach["filename"].get<string>();
+		size	= attach["size"].get<long>();
+		width	= attach["width"].get<long>();
+		height	= attach["height"].get<long>();
+	}
+};
+
+struct Message
+{
+	string author_id, channel_id, message_id;
+	string content, timestamp;
+	int attachments;
+	json raw_attachment;
+	Message(json msg)
+	{
+		author_id = msg["author"]["id"].get<string>();
+		channel_id = msg["channel_id"].get<string>();
+		message_id = msg["id"].get<string>();
+
+		content = msg["content"].get<string>();
+		timestamp = msg["timestamp"].get<string>();
+
+		attachments = msg["attachments"].size();
+
+		if(attachments > 0)
+		{
+			raw_attachment = msg["attachments"][0];
+		}
+	}
+	Attachment get_attachment()
+	{
+		if(attachments <= 0)
+			throw "No attachments!";
+		return Attachment(raw_attachment);
+	}
+};
+
 
 string token;
 static string submissions_id = "758776672186138624";
@@ -129,79 +173,71 @@ int main() {
 	bot->handlers.insert(
 			{
 				"MESSAGE_CREATE",
-				[&bot](json msg)
+				[&bot](json raw_msg)
 				{
-					string author_id = msg["author"]["id"].get<string>();
-					if(author_id == "758781959178289172")
+					Message msg(raw_msg);
+					if(msg.author_id == "758781959178289172")
 						return;
 					cout << "MESSAGE_CREATE\n";
-					string channel_id = msg["channel_id"].get<string>();
-					string message_id = msg["id"].get<string>();
-					string content = msg["content"].get<string>();
-					bool has_attachments = msg["attachments"].size() > 0;
-					bool in_submissions = channel_id == submissions_id;
-					bool in_error = channel_id == error_id;
-					string timestamp = msg["timestamp"].get<string>();
+					bool has_attachments = msg.attachments > 0;
+					bool in_submissions = msg.channel_id == submissions_id;
+					bool in_error = msg.channel_id == error_id;
 
 					// todo break if sender is ourselves
-					if(in_submissions && (!has_attachments || content.size() < 5))
+					if(in_submissions && (!has_attachments || msg.content.size() < 5))
 					{
-						delete_message(bot, channel_id, message_id);
+						delete_message(bot, msg.channel_id, msg.message_id);
 						send_message(bot, error_id,
-								"<@"+author_id+">, " + "All submissions must have both an attachment and a message of length >= 5. Your submission was deleted and not counted.");
+								"<@"+msg.author_id+">, " + "All submissions must have both an attachment and a message of length >= 5. Your submission was deleted and not counted.");
 					}
 					else if(in_submissions)
 					{
 						// add this submission to our database
-						json attachment = msg["attachments"][0];
-						string url = attachment["url"].get<string>();
-						long size = attachment["size"].get<long>();
-						long h = attachment["height"].get<long>();
-						long w = attachment["width"].get<long>();
-						string filename = attachment["filename"].get<string>();
-						db << "insert into submissions (author, link, filename, timestmp, width, height, size, description) values (?,?,?,?,?,?,?,?);"
-							<< author_id
-							<< url
-							<< filename
-							<< timestamp
-							<< w
-							<< h
-							<< size
-							<< content;
-						cout << "Successfully inserted new entry, " << author_id << " now has " << postcount(author_id) << endl;
+						Attachment attachment = msg.get_attachment();
+						db << "insert into submissions (author, timestmp, description, link, filename, width, height, size) values (?,?,?,?,?,?,?,?);"
+							<< msg.author_id
+							<< msg.timestamp
+							<< msg.content
+							<< attachment.url
+							<< attachment.filename
+							<< attachment.width
+							<< attachment.height
+							<< attachment.size;
+						cout << "Successfully inserted new entry, " << msg.author_id << " now has " << postcount(msg.author_id) << endl;
 
-						react(bot, submissions_id, message_id, "%F0%9F%86%97");
+						react(bot, submissions_id, msg.message_id, "%F0%9F%86%97");
 					}
 					else if(in_error)
 					{
-						istringstream iss(content);
+						istringstream iss(msg.content);
 						vector<string> tokens{istream_iterator<string>{iss}, istream_iterator<string>{}};
 						string datastring="err";
 						if(tokens.size() == 1)
 						{
 							if(tokens[0] == "postcount")
 							{
-								long cnt = postcount(author_id);
+								long cnt = postcount(msg.author_id);
 								datastring = "You have made " + to_string(cnt) + " posts.";
 							}
-							else if(tokens[0] == "lastpost" && postcount(author_id) > 0)
+							else if(tokens[0] == "lastpost" && postcount(msg.author_id) > 0)
 							{
-								string url = latestposturl(author_id);
-								string desc = latestpostdescription(author_id);
+								string url = latestposturl(msg.author_id);
+								string desc = latestpostdescription(msg.author_id);
 								datastring = desc + "\n" + url;
 							}
 						}
 						send_message(bot, error_id,
-								"<@"+author_id+">, " + datastring);
+								"<@"+msg.author_id+">, " + datastring);
 					}
-					cout << msg << endl;
+					cout << raw_msg << endl;
 				}
 			});
 	auto aioc = std::make_shared<asio::io_context>();
-	cout << "Initialization\n";
-	bot->initBot(6, token, aioc);
 
-	cout << "Running\n";
+	bot->initBot(6, token, aioc);
+	cout << "Initialization complete\n";
+
+	cout << "Running...\n";
 	bot->run();
 	return 0;
 }
